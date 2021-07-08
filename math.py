c89fc99b0fa820af5a67d3a6e863312535d9955c
Divagaran <53164426+rajadivagaran@users.noreply.github.com>
2021-07-08 14:28:37 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,19 @@
+# add Implementation
+def add(x,y):
+    return x+y;
+# sub Implementation
+def subtract(x,y):
+    return x-y;
+# multiply Implementation
+def multiply(x,y):
+    return x*y
+# div Implementation
+def divide(x,y):
+    if y==0:
+        return 'Error_code'
+    else:
+        return x/y;
+# sqr implement
+def square(x):
+    pass
+
